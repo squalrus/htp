a0b02dd1879554575516920c95af032a11e5a2c4
Chad Schulz <chad.awesome@gmail.com>
2013-07-17 00:28:46 -0700

    Includes the Foursquare config, now defined externally.

---
+++ htp/htp-foursquare.js
@@ -0,0 +1,11 @@
+var nconf = require( 'nconf' );
+
+nconf.env().file({ file: 'config.json' });
+
+exports.foursquareOptions = {
+    'secrets': {
+          'clientId':       nconf.get( '4SQ_CLIENT_ID' )
+        , 'clientSecret':   nconf.get( '4SQ_CLIENT_SECRET' )
+        , 'redirectUrl':    nconf.get( '4SQ_REDIRECT_URL' )
+    }
+};
