ae7467fb5a62416da6bca2c0b41798d2aa76c2c0
Chad Schulz <chad.awesome@gmail.com>
2013-12-27 11:43:00 -0800

    Normalize things

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,57 +1,57 @@
-module.exports = function( grunt ){
+module.exports = function (grunt) {
 
     // Project Configuration
     grunt.initConfig({
-         pkg: grunt.file.readJSON( 'package.json' )
+        pkg: grunt.file.readJSON("package.json"),
 
          // Uglify JS
-        ,uglify: {
-             options: { }
-            ,dist: {
+        uglify: {
+            options: {},
+            dist: {
                 files: {
-                     // 'public/dest/file.js' : ['source.js']
+                     // "public/dest/file.js" : ["source.js"]
                 }
             }
-        }
+        },
 
         // Compile LESS
-        ,less: {
+        less: {
             development: {
                 options: {
                     compress: true
-                }
-                ,files: {
-                     // 'public/css/dest.min.css': 'source.less'
+                },
+                files: {
+                     // "public/css/dest.min.css": "source.less"
                 }
             }
-        }
+        },
 
         // jshint files/directories
-        ,jshint: {
-            all: ['htp/*.js', 'models/*.js','routes/*.js']
-            ,options: {
+        jshint: {
+            all: ["htp/*.js", "models/*.js","routes/*.js"],
+            options: {
                 laxcomma: true,
                 eqeqeq: true,
-                quotmark: 'single',
+                quotmark: "single",
                 unused: true,
                 strict: true,
                 trailing: true
             }
-        }
+        },
 
         // Watch Directories / Files
-        ,watch: {
-             files: ['htp/*.js', 'models/*.js','routes/*.js']
-            ,tasks: ['default']
+        watch: {
+            files: ["htp/*.js", "models/*.js","routes/*.js"],
+            tasks: ["default"]
         }
     });
 
     // Load the plugins
-    grunt.loadNpmTasks( 'grunt-contrib-uglify' );
-    grunt.loadNpmTasks( 'grunt-contrib-less' );
-    grunt.loadNpmTasks( 'grunt-contrib-jshint' );
-    grunt.loadNpmTasks( 'grunt-contrib-watch' );
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-contrib-less");
+    grunt.loadNpmTasks("grunt-contrib-jshint");
+    grunt.loadNpmTasks("grunt-contrib-watch");
 
     // Default tasks
-    grunt.registerTask( 'default', ['uglify', 'less', 'jshint'] );
+    grunt.registerTask("default", ["uglify", "less", "jshint"]);
 };
--- server.js
+++ server.js
@@ -1,75 +1,72 @@
-var  express       = require( 'express' )
-    ,http          = require( 'http' )
-    ,path          = require( 'path' )
-    ,azure         = require( 'azure' )
-    ,nconf         = require( 'nconf' )
-    ,htp           = require( './htp/htp-foursquare' )
-    ,foursquare    = require( 'node-foursquare' )( htp.foursquareOptions )
-    ,app           = express()
-    ;
+var express       = require("express"),
+    http          = require("http"),
+    path          = require("path"),
+    azure         = require("azure"),
+    nconf         = require("nconf"),
+    htp           = require("./htp/htp-foursquare"),
+    foursquare    = require("node-foursquare")(htp.foursquareOptions),
+    app           = express();
 
 // Routing
-var  routes        = require( './routes' )
-     api           = require( './routes/api' )
-    ;
+var routes        = require( "./routes" ),
+    api           = require( "./routes/api" );
 
-nconf.env().file({ file: 'config.json' });
+nconf.env().file({ file: "config.json" });
 
-var  tableName     = nconf.get( 'TABLE_NAME' )
-    ,partitionKey  = nconf.get( 'PARTITION_KEY' )
-    ,accountName   = nconf.get( 'STORAGE_NAME' )
-    ,accountKey    = nconf.get( 'STORAGE_KEY' )
-    ;
+var tableName     = nconf.get("TABLE_NAME"),
+    partitionKey  = nconf.get("PARTITION_KEY"),
+    accountName   = nconf.get("STORAGE_NAME"),
+    accountKey    = nconf.get("STORAGE_KEY");
 
 // Environment Settings
-app.set( 'port', process.env.PORT || 3000 );
-app.set( 'views', __dirname + '/views' );
-app.set( 'view engine', 'ejs' );
+app.set("port", process.env.PORT || 3000);
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
 
 // Middleware
-app.use( express.favicon() );
-app.use( express.bodyParser() );
-app.use( express.compress() );
-app.use( express.cookieParser() );
-app.use( express.cookieSession({ key: 'pid', secret: 'pissr' }) );
-app.use( express.static( path.join( __dirname, 'public' ) ) );
+app.use(express.favicon());
+app.use(express.bodyParser());
+app.use(express.compress());
+app.use(express.cookieParser());
+app.use(express.cookieSession({ key: "pid", secret: "pissr" }));
+app.use(express.static(path.join( __dirname, "public" )));
 
 /*
  * Page Routing
  * --------------------------------------------------------------- */
  // Index
-app.get( '/', routes.index );
+app.get("/", routes.index);
 // List
-app.get( '/list', routes.list );
+app.get("/list", routes.list);
 // Venues
-app.get( '/venues', routes.list );
+app.get("/venues", routes.list);
 // Login
-app.get( '/login', routes.login );
+app.get("/login", routes.login);
 // Insert
-app.get( '/insert', function( req, res ){
+app.get("/insert", function (req, res) {
     var tableService = azure.createTableService();
 
-    // tableService.queryEntity( 'pissers', )
+    // tableService.queryEntity("pissers", )
 });
 // Callback
-app.get( '/callback', routes.callback );
+app.get("/callback", routes.callback);
 // Callback
-app.get( '/test', routes.test );
+app.get("/test", routes.test);
 
 /*
  * API Routing
  * --------------------------------------------------------------- */
 // Venues
-app.get( '/api/venue', api.venues );
+app.get("/api/venue", api.venues);
 
 // Venue Details
-app.get( '/api/venue/:id', api.venue );
-app.post( '/api/venue/:id', api.rate );
+app.get("/api/venue/:id", api.venue);
+app.post("/api/venue/:id", api.rate);
 
 // User Details
-app.get( '/api/user/:id', api.user );
+app.get("/api/user/:id", api.user);
 
 // Create Server
-http.createServer( app ).listen( app.get( 'port' ), function( ){
-    console.log( 'Server listening on port ' + app.get( 'port' ) );
+http.createServer(app).listen(app.get("port"), function () {
+    console.log("Server listening on port " + app.get("port"));
 });
